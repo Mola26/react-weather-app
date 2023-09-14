f95699ea30af882a6c5dddfd295fb839e1ee412b
Mola26 <nokuthaba2608@gmail.com>
2023-09-14 02:37:39 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        Created with love by Nokuthaba Siphambili and is
+        <a href="https://github.com/Mola26/react-weather-app" target="_blank">
+          {" "}
+          open soured on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
