cbf7ba1545bb86e6fc267687e4ee1bf9a237b7e2
Mola26 <nokuthaba2608@gmail.com>
2023-09-14 05:33:59 +0200

    Added axios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Pretoria" />
 
         <footer>
           Created with love by Nokuthaba Siphambili and is
