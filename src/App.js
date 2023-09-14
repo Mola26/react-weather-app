53d2d346e39471a465fc958580ad023bdcad81e4
Mola26 <nokuthaba2608@gmail.com>
2023-09-14 05:35:41 +0200

    Changed city name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Pretoria" />
+        <Weather defaultCity="Bulawayo" />
 
         <footer>
           Created with love by Nokuthaba Siphambili and is
