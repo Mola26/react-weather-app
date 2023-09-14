62ff21b96c41d6aacda8e61db7d8041de3fedbb8
Mola26 <nokuthaba2608@gmail.com>
2023-09-14 02:53:38 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        Created with love by Nokuthaba Siphambili and is
-        <a
-          href="https://github.com/Mola26/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open soured on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <button>Hello</button>
+        <footer>
+          Created with love by Nokuthaba Siphambili and is
+          <a
+            href="https://github.com/Mola26/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open soured on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
